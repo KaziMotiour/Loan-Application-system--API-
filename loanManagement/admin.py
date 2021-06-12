4763a9b17aa45c4d6842253a8d708239f42f31d4
KaziMotiour <kmatiour30@gmail.com>
2021-06-12 21:17:01 +0600

    empty file

--- loanManagement/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Bank, BankBranch, Application
-
-# Register your models here.
-admin.site.register(Bank)
-admin.site.register(BankBranch)
-admin.site.register(Application)
--- make_payment/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MakePaymentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'make_payment'
